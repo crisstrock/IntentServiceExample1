1323859dfbf58d466a9ad2f8a52468dd4c3dadd2
Cris <cristianruiz1195@gmail.com>
2019-10-27 18:46:08 -0600

    First commit fot test

---
+++ app/src/main/java/com/crisstrock/intentserviceexample1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.crisstrock.intentserviceexample1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v){
+        String input = editTextInput.getText().toString().trim();
+        Intent serviceIntent = new Intent(this, ExampleIntentService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+}
